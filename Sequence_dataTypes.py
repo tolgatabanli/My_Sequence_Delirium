7087ad86b6ff480f6a632dc24eafa3690fcf7b81
tolgatabanli <tolgatabanli@gmail.com>
2023-10-17 23:15:53 +0200

    Added new functionality

--- Sequence_dataTypes.py
+++ Sequence_dataTypes.py
@@ -136,3 +136,24 @@ class ProtSeq:
         for k in helixseq:
             helixhydropathy += aminoacid_hydropathy[k]
         return helixhydropathy
+
+    def helix_number(self, threshold=4):
+        helixed_order = self.helical_wheel()
+        helixcount = 0
+        window_cont = False
+        window_break = 0
+        for k in range(len(helixed_order)):
+            a = helixed_order[k:k+threshold]
+            if a == "P"*threshold or a == "H"*threshold:
+                if window_cont:
+                    window_cont = True
+                else:
+                    helixcount += 1
+                    window_cont = True
+            else:
+                if window_break <= 4:
+                    window_break += 1
+                else:
+                    window_cont = False
+                    continue
+        return helixcount
--- main.py
+++ main.py
@@ -4,12 +4,16 @@ print("This program allows you to do following:\n"
       "1. Convert protein coding sequence to protein sequence\n"
       "2. Order protein sequence linearly in helical wheel and represent aminoacids according to hydropathy "
       "(H: hydrophilic, M: moderate, P: hydrophobic)\n"
-      "")
+      "3. Analyze number of predicted helix structures inside given protein sequence\n")
 process = input("Enter the number of the process you'd like to proceed with: ")
 
-if process == 1:
-    seqinput = ProtCodingSeq(input("Enter your protein coding sequence: "))
-    print(seqinput.codon_to_prot())
-elif process == 2:
-    protseq = ProtSeq(input("enter prot seq: "))
-    print(protseq.helical_wheel())
+if int(process) == 1:
+    seq = ProtCodingSeq(input("Enter your protein coding sequence: "))
+    print(seq.codon_to_prot())
+elif int(process) == 2:
+    seq = ProtSeq(input("enter prot seq: "))
+    print(seq.helical_wheel())
+elif int(process) == 3:
+    seq = ProtSeq(input("Enter protein sequence: "))
+    thr = int(input("Enter threshold number with which similar hydropathy blocks should be detected: "))
+    print(seq.helix_number(thr))
